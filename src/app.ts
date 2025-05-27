2b7f35fd676cedffd323f9a106843a83f15833b2
Brais de Sousa <braisdesousa@gmail.com>
2025-05-27 10:17:59 +0200

    [WORKAROUND] Buscar el fallo en las rutas

--- src/app.ts
+++ src/app.ts
@@ -51,7 +51,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
-    console.log('Variables de entorno cargadas:', {
+    console.log('Variables de entorno cagadas:', {
         PORT: process.env.PORT,
         BASE_URL: process.env.BASE_URL,
         FRONTEND_URL: process.env.FRONTEND_URL,
